0da85730aae745c456af3cb5121b0dd151a8c815
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-09-15 13:38:18 -0600

    ensure EmailValidator returns false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,8 +1,9 @@
 /* eslint-env jest */
+const validator = require('validator')
 
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
@@ -11,5 +12,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+    expect(isEmailValid).toBe(false)
   })
 })
