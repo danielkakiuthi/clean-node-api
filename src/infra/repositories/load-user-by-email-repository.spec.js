1016c86da30c00f1090a4a5a50e075b26fd0c719
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-09-27 14:46:41 -0600

    ensure LoadUserByEmailRepository returns an user with id and password only

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -8,7 +8,13 @@ class LoadUserByEmailRepository {
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne(
+      { email }, {
+        projection: {
+          password: 1
+        }
+      }
+    )
     return user
   }
 }
@@ -48,10 +54,18 @@ describe('LoadUserByEmail Repository', () => {
 
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
+    const fakeUser = {
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
+    }
+    const responseUser = await userModel.insertOne(fakeUser)
+    const user = await sut.load(fakeUser.email)
+    expect(user).toEqual({
+      _id: responseUser.insertedId,
+      password: fakeUser.password
     })
-    const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
   })
 })
